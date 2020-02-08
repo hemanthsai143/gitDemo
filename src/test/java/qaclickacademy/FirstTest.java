dda1ea38862af5c3141dc3c1886711577b0eea33
hemanthsai143 <hemanthsaibommuluri35@gmail.com>
2020-02-08 12:39:02 +0530

    MyFirstCommit

---
+++ src/test/java/qaclickacademy/FirstTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	
+	@Test
+	public void firstone()
+	{
+		System.out.println("hi how are you");
+	}
+	
+	@Test							
+	public void secondone()
+	{
+		System.out.println("I am Good");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/SecTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SecTest {
+	
+	@Test
+	public void fiestex()
+	{
+		System.out.println("I am Good");
+	}
+    
+	@Test
+	public void sconfis()
+	{
+		System.out.println("how about you");
+	}
+}
